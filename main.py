292b804a27e931c731aa5c2d6783abe798344fc9
dean_hsu <dean_hsu@paradise-soft.com.tw>
2022-06-16 11:25:54 +0800

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from re import sub
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import os
+import time
+
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+option.add_argument('window-size=1920x1080')
+
+# Configs
+chrome_driver = "./chromedriver"
+account = ""
+pwd = ""
+productUrl = "https://p-bandai.com/tw/item/N2631684001001"
+
+# open product page
+driver = webdriver.Chrome(chrome_options=option, executable_path=chrome_driver)
+driver.get(productUrl)
+
+# login flow
+loginBtn = driver.find_element_by_xpath("/html/body/header/div[1]/div[4]/a")
+loginBtn.click()
+
+accountElem = driver.find_element_by_xpath("/html/body/div[1]/div/main/section/form/div[2]/div[1]/section/div[2]/div[1]/div[1]/div[2]/label/input")
+accountElem.send_keys(account)
+pwdElem = driver.find_element_by_xpath("/html/body/div[1]/div/main/section/form/div[2]/div[1]/section/div[2]/div[1]/div[2]/div[2]/label/input")
+pwdElem.send_keys(pwd)
+
+submitLoginBtn = driver.find_element_by_xpath("/html/body/div[1]/div/main/section/form/div[2]/div[1]/section/div[2]/div[2]/button/a")
+submitLoginBtn.click()
+
+# redirect to product
+driver.get(productUrl)
+
+# select quantity
+countSelect = Select(driver.find_element_by_xpath("/html/body/div[1]/div/main/section/section[1]/div[1]/div[2]/form/ul/li/div/select"))
+countSelect.select_by_value("2")
+
+# add to cart
+submitBtn = driver.find_element_by_xpath("/html/body/div[1]/div/main/section/section[1]/div[1]/div[2]/form/div/button")
+submitBtn.click()
+
+driver.get_screenshot_as_file("capture.png")
